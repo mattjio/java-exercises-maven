b562dec4ed5be8bc1adfd3119a9388ced93b4507
matthew jio <matthew.p.jio1@gmail.com>
2022-07-01 13:46:03 -0500

    fix

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Bob {
 
 
-    public static void main(Bob[] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
