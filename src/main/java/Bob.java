386d55632079b4f58297be3ce5de1f1d29ee7ae2
matthew jio <matthew.p.jio1@gmail.com>
2022-07-01 12:07:42 -0500

    maven

---
+++ src/main/java/Bob.java
@@ -0,0 +1,26 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Bob {
+
+
+    public static void main(Bob[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        java.lang.String str = sc.nextLine();
+        System.out.println("You entered: " + str);
+        if (StringUtils.isNumeric(str)) {
+            System.out.println(str+" is a number");
+        } else {
+            System.out.println(str+" is not a number");
+        }
+        String flipCase = StringUtils.swapCase(str);
+        System.out.println("Flipped Case: " + flipCase);
+        String reverse = StringUtils.reverse(str);
+        System.out.println("Reversed: " + reverse);
+    }
+
+}
